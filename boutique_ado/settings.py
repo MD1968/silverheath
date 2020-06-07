5de4585354ad2da695a111bf1f55b3c2d3760d14
Mark Dyson <mark.dyson1@outlook.com>
2020-06-07 16:08:32 +0000

    m

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -200,4 +200,4 @@ STRIPE_CURRENCY = 'GBP'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+DEFAULT_FROM_EMAIL = 'mark@silverheath.co.uk'
