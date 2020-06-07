b25c90d29b8c285863bbba0ca25b1e36681d6baa
Mark Dyson <mark.dyson1@outlook.com>
2020-06-07 13:01:21 +0000

    m

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['silverheath.herokuapp.com']
 
 
 # Application definition
